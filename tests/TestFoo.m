75cd215eff29f339b5910f2dfb215f99f34c213b
Aaron Ward <award7@wisc.edu>
2024-09-20 20:38:16 -0400

    fix: rm exploration files

--- tests/TestFoo.m
+++
@@ -1,9 +0,0 @@
-classdef TestFoo < matlab.unittest.TestCase
-    methods(Test)
-        function test_foo(testCase)
-            expected = "Hello, world";
-            actual = foo();
-            testCase.verifyEqual(actual, expected);
-        end
-    end
-end
